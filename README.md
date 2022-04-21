471c0c9535da29700aa8718d0786ef516950011e
Shailendra Murmu <shailmurmu1996@gmail.com>
2022-04-21 13:06:36 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+// Crypto Watcher  - A simple ReactJS aplcation that helps to track latest trending cryptocurrency.
+// It also allow you to save your favorite crypto in your watchlist after you logged into application.
+
+
+Enjoy !!
---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCbBKEH3_RIWi-O7NOCWfZFbz7NYbFU1L0",
+  authDomain: "crypto-watcher-a8eb5.firebaseapp.com",
+  projectId: "crypto-watcher-a8eb5",
+  storageBucket: "crypto-watcher-a8eb5.appspot.com",
+  messagingSenderId: "540951882746",
+  appId: "1:540951882746:web:c85a1329251a4ff2479243",
+  measurementId: "G-C6T3T7HLW9"
+};
+
+export default firebaseConfig;
